81d8701064aef06dcb3cb62392edf9d59272fd69
veena <Veena@BB-2710.com>
2026-04-03 15:46:48 +0530

    added search bar

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
 
+admin.site.register(Product, ProductAdmin)
